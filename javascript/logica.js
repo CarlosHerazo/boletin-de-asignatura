4757496a98ceb16f6bc68930062ddf27430eb74e
CarlosHerazo <Carlosherazo2004@gmail.com>
2023-08-25 08:39:48 -0500

    primer commit

---
+++ javascript/logica.js
@@ -0,0 +1,10 @@
+document.getElementById("buttonGenerate").addEventListener("click", ()=>{
+    let myName = document.getElementById("inputTextName").value;
+    let lastName = document.getElementById("inputTextLastName").value;
+    let identification = document.getElementById("inputNumberIdentication").value;
+    console.log(myName);
+    console.log(lastName);
+    console.log(identification);
+});
+
+
